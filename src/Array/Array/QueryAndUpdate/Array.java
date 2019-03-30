c8dae212654ffbf6a9aa8ca2ddb66d1dd7c5c26d
Unknown <meteorboy321@gmail.com>
2019-03-30 12:40:47 +0800

    a

--- src/Array/Array/QueryAndUpdate/Array.java
+++ src/Array/Array/QueryAndUpdate/Array.java
@@ -7,6 +7,7 @@ public class Array {
     private int size;
 
     // 構造函數，傳入數組的容量capacity構造Array //456change
+    //a
     public Array(int capacity){
         data = new int[capacity];
         size = 0;
